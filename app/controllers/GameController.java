f65f80b09e31e49f531f551ac03eb42bd3970baa
Sabrina Runge <thesabrinarunge@gmail.com>
2018-08-29 13:44:54 -0500

    Northwest Passage

--- app/controllers/GameController.java
+++ app/controllers/GameController.java
@@ -10,8 +10,8 @@ import javax.inject.Inject;
 
 public class GameController extends Controller
 {
+    private final String remainingCrewSessionKey = "remainingcrew";
     private FormFactory formFactory;
-    private String name;
 
     @Inject
     public GameController(FormFactory formFactory)
@@ -27,46 +27,98 @@ public class GameController extends Controller
     public Result postStart()
     {
         DynamicForm form = formFactory.form().bindFromRequest();
-        name = form.get("playername");
-        Logger.info("playername" + name);
+        String name = form.get("playername");
+        Logger.info("form playername: " + name);
         if(name != null)
         {
             session().put("playername", name);
         }
+
+        String nameFromSession = session().get("playername");
+        Logger.info("session playername: " + nameFromSession);
+
+        int numberOfCrew = 10;
+        session().put(remainingCrewSessionKey, "" + numberOfCrew);
 
         return ok(views.html.start.render());
     }
 
     public Result postEastFromEngland()
     {
-        return ok(views.html.eastfromengland.render());
+        String name = session().get("playername");
+        int numberOfCrew = Integer.parseInt(session().get(remainingCrewSessionKey));
+        numberOfCrew--;
+        session().put(remainingCrewSessionKey, "" + numberOfCrew);
+
+        boolean failure = false;
+
+        if (numberOfCrew < 5)
+        {
+            failure = true;
+            return ok(views.html.oakisland.render(name));
+        }
+
+        else
+        {
+            failure = false;
+            return ok(views.html.eastfromengland.render("" + numberOfCrew));
+        }
     }
 
     public Result postNorthFromEngland()
     {
+        String name = session().get("playername");
         return ok(views.html.northfromengland.render(name));
     }
 
     public Result postWestFromEngland()
     {
-        return ok(views.html.westfromengland.render());
+        String name = session().get("playername");
+        int numberOfCrew = Integer.parseInt(session().get(remainingCrewSessionKey));
+        numberOfCrew--;
+        session().put(remainingCrewSessionKey, "" + numberOfCrew);
+
+        boolean failure = false;
+
+        if (numberOfCrew < 5)
+        {
+            failure = true;
+            return ok(views.html.oakisland.render(name));
+        }
+
+        else
+        {
+            failure = false;
+            return ok(views.html.westfromengland.render("" + numberOfCrew));
+        }
     }
 
-    public Result postKeepGoingNorth(){return ok(views.html.keepgoingnorth.render());}
+    public Result postKeepGoingNorth()
+    {
+        return ok(views.html.keepgoingnorth.render());
+    }
 
     public Result postEastEnd()
     {
+        String name = session().get("playername");
         return ok(views.html.eastend.render(name));
     }
 
     public Result postWestEnd()
     {
+        String name = session().get("playername");
         return ok(views.html.westend.render(name));
     }
 
     public Result postHomePort()
     {
         return ok(views.html.homeport.render());
+    }
+
+    public Result postOakIsland()
+    {
+        String name = session().get("playername");
+        return ok(views.html.oakisland.render(name));
     }
 
     public Result getKittens()
